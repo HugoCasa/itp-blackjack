1473f96150f54da495c746ecdd74b466b280c5c0
HugoCasa <hugo@casademont.ch>
2019-12-18 16:44:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,32 +55,57 @@ class Player:
     def __init__(self, chips=0):
         self.chips = chips
         self.cards = []
+        self.splitCards = []
         self.aces = 0
+        self.splitAces = 0
 
-    def draw(self, deck):
-        tempCard = deck.draw()
-        self.cards.append(tempCard)
-        if tempCard.value == 11:
-            self.aces += 1
+    def draw(self, deck, split=False):
+        # tempCard = deck.draw()
+        tempCard = Card("Hearts", 8)
+        if split:
+            self.splitCards.append(tempCard)
+            if tempCard.value == 11:
+                self.splitAces += 1
+        else:
+            self.cards.append(tempCard)
+            if tempCard.value == 11:
+                self.aces += 1
 
-    def showCards(self):
-        for c in self.cards:
-            c.show()
+    def showCards(self, split=False):
+        if not split:
+            for c in self.cards:
+                c.show()
+        else:
+            for c in self.splitCards:
+                c.show()
 
     def showChips(self):
         print("You currently have %d chips" % self.chips)
 
     def bet(self, amt, pot):
-        self.chips -= amt
-        pot.add(amt)
+        if amt > self.chips:
+            print("You don't have enough money!")
+            return False
+        else:
+            self.chips -= amt
+            pot.add(amt)
+            return True
 
-    def wins(self, pot):
-        print("You won!")
-        self.chips += pot.chips
+    def wins(self, pot, share=1):
+        amt = share*pot.chips
+        print("+ %d chips" % amt)
+        self.chips += amt
 
-    def getCardsValue(self):
-        val = sum(c.value for c in self.cards)
-        remainingAces = self.aces
+    def getCardsValue(self, split=False):
+        val = 0
+        remainingAces = 0
+        if split:
+            val = sum(c.value for c in self.splitCards)
+            remainingAces = self.splitAces
+        else:
+            val = sum(c.value for c in self.cards)
+            remainingAces = self.aces
+
         while val > 21 and remainingAces != 0:
             val = val - 10
             remainingAces -= 1
@@ -88,6 +113,13 @@ class Player:
             return 0
         else:
             return(val)
+
+    def split(self):
+        self.splitCards.append(self.cards.pop())
+        for c in self.splitCards:
+            if c.value == 11:
+                self.splitAces += 1
+                self.aces -= 1
 
 
 class Pot:
@@ -101,20 +133,50 @@ class Pot:
 def betting(player, dealer, pot):
     player.showChips()
     try:
-        bet = input("How much do you want to bet? ")
-        bet = int(bet)
-        player.bet(bet, pot)
+        enoughMoney = False
+        while not enoughMoney:
+            bet = input("How much do you want to bet? ")
+            bet = int(bet)
+            enoughMoney = player.bet(bet, pot)
         dealer.bet(bet, pot)
+        print("- %d chips" % bet)
     except:
         print("Please enter a valid number")
+
+
+def splitStrategy(player, deck, secondHand=False):
+    if secondHand:
+        print("Second Hand:")
+    else:
+        print("First Hand:")
+    player.showCards(split=secondHand)
+    print("1) Hit")
+    print("2) Stand")
+    try:
+        choice = input("What would you like to do? ")
+        choice = int(choice)
+        if choice == 1:
+            player.draw(deck, split=secondHand)
+            player.showCards(split=secondHand)
+            return not player.getCardsValue(split=secondHand)
+        elif choice == 2:
+            return True
+        else:
+            print("Please enter a number between 1 and 2")
+            return False
+    except:
+        print("Please enter a number between 1 and 2")
 
 
 def strategy(player, dealer, deck, pot):
     print("1) Hit")
     print("2) Stand")
-    print("3) Double")
-    if len(player.cards) == 2 and player.cards[0].number == player.cards[1].number:
-        print("4) Split")
+    if len(player.cards) == 2 and not player.splitCards:
+        if player.chips >= pot.chips/2:
+            print("3) Double")
+            if player.cards[0].number == player.cards[1].number:
+                print("4) Split")
+
     try:
         choice = input("What would you like to do? ")
         choice = int(choice)
@@ -124,12 +186,21 @@ def strategy(player, dealer, deck, pot):
             return not player.getCardsValue()
         elif choice == 2:
             return True
-        elif choice == 3 and len(player.cards) == 2:
-            player.bet(pot.chips/2, pot)
-            dealer.bet(pot.chips/2, pot)
+        elif choice == 3:
+            doubleBet = pot.chips/2
+            player.bet(doubleBet, pot)
+            dealer.bet(doubleBet, pot)
+            print("- %d chips" % doubleBet)
             player.draw(deck)
             player.showCards()
             return True
+        elif choice == 4:
+            doubleBet = pot.chips/2
+            player.split()
+            player.bet(doubleBet, pot)
+            dealer.bet(doubleBet, pot)
+            print("- %d chips" % doubleBet)
+            return False
         else:
             print("Please enter a number between 1 and 4")
             return False
@@ -138,11 +209,21 @@ def strategy(player, dealer, deck, pot):
         print("Please enter a number between 1 and 4")
 
 
-def dealerStrategy(player, deck):
-    val = player.getCardsValue()
-    while val <= 16:
-        player.draw(deck)
-        val = player.getCardsValue()
+def dealerStrategy(dealer, deck):
+    val = dealer.getCardsValue()
+    while val <= 16 and val > 0:
+        dealer.draw(deck)
+        val = dealer.getCardsValue()
+
+
+def outcome(playerCardsValue, dealerCardsValue):
+    if not dealerCardsValue or playerCardsValue > dealerCardsValue:
+        if playerCardsValue == 21:
+            print("You have BlackJack!")
+        return True
+    else:
+        print("You lost!")
+        return False
 
 
 def game(player):
@@ -171,18 +252,40 @@ def game(player):
 
     # loop trough decision taking
     done = False
+    secondHand = False
     while not done:
-        done = strategy(player, dealer, deck, pot)
+        if not player.splitCards:
+            done = strategy(player, dealer, deck, pot)
+        else:
+            if not secondHand:
+                secondHand = splitStrategy(player, deck, secondHand)
+            else:
+                done = splitStrategy(player, deck, secondHand)
 
     # check whether player wins or not
-    cardsValue = player.getCardsValue()
-    if cardsValue:
+    playerCardsValue = player.getCardsValue()
+    splitCardsValue = player.getCardsValue(split=True)
+    if playerCardsValue or splitCardsValue:
         dealerStrategy(dealer, deck)
         print("Dealer's cards:")
         dealer.showCards()
-        if (cardsValue > dealer.getCardsValue()) or not dealer.getCardsValue():
-            if cardsValue == 21:
-                print("BlackJack")
+
+        firstOutcome = outcome(playerCardsValue, dealer.getCardsValue())
+
+        if splitCardsValue:
+            splitOutcome = outcome(splitCardsValue, dealer.getCardsValue())
+            if playerCardsValue and firstOutcome:
+                print("You won your first hand!")
+                player.wins(pot, share=0.5)
+            else:
+                print("You lost your first hand!")
+            if splitCardsValue and splitOutcome:
+                print("You won your second hand!")
+                player.wins(pot, share=0.5)
+            else:
+                print("You lost your second hand!")
+        elif firstOutcome:
+            print("You won")
             player.wins(pot)
         else:
             print("You lost!")
